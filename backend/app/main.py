0009c2aa0e460fb1a87ce317eba63b89025f6554
Teodora <teodora.manjencic@dex.rs>
2026-09-18 08:44:20 +0200

    Implement PDF upload API and Base64 conversion

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,34 @@
-from fastapi import FastAPI
+curl -X POST "http://localhost:8000/upload" \
+  -F "file=@test.pdf.pdf"from fastapi import FastAPI, UploadFile, File
+import os
+import base64
+
+UPLOAD_DIR = "uploads"
 
 app = FastAPI()
+
+
 @app.get("/")
 def home():
     return {"AIxRPA API radi! :)"}
+
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+    file_path = f"{UPLOAD_DIR}/{file.filename}"
+
+    content = await file.read()
+
+    pdf_base64 = base64.b64encode(content).decode("utf-8")
+
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+    return {
+        "filename": file.filename,
+        "base64length": len(pdf_base64)
+    }
+
+
