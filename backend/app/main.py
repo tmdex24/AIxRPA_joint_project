08780da3eba279bcc94a0afd1a92ab1789004d6d
Teodora <teodora.manjencic@dex.rs>
2026-09-21 15:02:24 +0200

    obrisan database folder iz backenda, bio bespotreban

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,9 +15,11 @@ UPLOAD_DIR = "uploads"
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"AIxRPA API radi! :)"}
+
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
@@ -56,17 +58,17 @@ async def upload_pdf(file: UploadFile = File(...)):
 
     if date:
 
-    for fmt in ("%d/%m/%Y", "%Y-%m-%d"):
+        for fmt in ("%d/%m/%Y", "%Y-%m-%d"):
 
-        try:
-            invoice_data["date_of_issue"] = (
-                datetime.strptime(date, fmt)
-                .strftime("%Y-%m-%d")
-            )
-            break
+            try:
+                invoice_data["date_of_issue"] = (
+                    datetime.strptime(date, fmt)
+                    .strftime("%Y-%m-%d")
+                )
+                break
 
-        except ValueError:
-            passstrftime("%Y-%m-%d")
+            except ValueError:
+                pass
 
     update_invoice_data(
         invoice_id,
@@ -79,3 +81,9 @@ async def upload_pdf(file: UploadFile = File(...)):
         invoice_data.get("currency"),
         "COMPLETED"
     )
+
+    return {
+        "invoice_id": invoice_id,
+        "filename": file.filename,
+        "invoice": invoice_data
+    }
